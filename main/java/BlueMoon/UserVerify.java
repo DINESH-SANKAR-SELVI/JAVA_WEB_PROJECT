e4b7be5e9f448fb5ca9f08e5f4c8bbd47347fdf5
DINESH-SANKAR-SELVI <ddprofessional31@gmail.com>
2023-02-01 08:26:52 -0800

    FIRST COMMIT

--- main/java/BlueMoon/UserVerify.java
+++ main/java/BlueMoon/UserVerify.java
@@ -18,7 +18,6 @@ public class UserVerify extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		PrintWriter io = response.getWriter();
-		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BlueMoon?characterEncoding=latin1","root","White@Kite_0110.");
@@ -36,9 +35,6 @@ public class UserVerify extends HttpServlet {
 			else { io.println("<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>Document</title></head>");
 				   io.println("<body style='position: absolute;top:40%;right:40%;background-color: rgba(255,0,0,0.4);'><h1 style='text-transform:uppercase;'>tryagain......!</h1><pre>      Click here<a style='text-decoration: none;' href='LogInPage.jsp' ><b> to LogIn</b></a></pre></body></html>");
 			}
-			
-			
-			
 		}catch(Exception e){e.printStackTrace();}
 	}
 
